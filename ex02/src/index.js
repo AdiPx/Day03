e1ced3af4e2fdc3e33d062a3f2561e04f16d4f2a
Adipx <adipasic86@gmail.com>
2021-03-10 16:45:29 +0100

    added index.js (comments)

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code below this line
+    // comment with at least 5 letters
+    /*another comment also with at least 5 letters*/
+    // Only change code above this line
+}
+
+console.log (main());
+module.exports = main;
